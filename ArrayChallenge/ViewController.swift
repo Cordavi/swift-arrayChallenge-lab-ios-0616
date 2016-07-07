b624d420e4a7ab221c3de4702cd455aef25886e3
Cordavi <michaelamundsen@gmail.com>
2016-07-07 13:15:45 -0400

    Completed lab so test pass. Wrote func to combine two arrays into one.

--- ArrayChallenge/ViewController.swift
+++ ArrayChallenge/ViewController.swift
@@ -11,15 +11,26 @@ import UIKit
 class ViewController: UIViewController {
     
     var shoppingList: [String] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let quantityArray = [6, 4, 12, 4]
+        let itemsArray = ["Bananas", "Apples", "Eggs", "Rolls"]
+        shoppingList = makeShoppingList(itemsArray, quantityOfItems: quantityArray)
     
-        
         
     }
     
-    
+    func makeShoppingList(items:[String], quantityOfItems: [Int]) -> [String] {
+        var combinedArray: [String] = []
+        if quantityOfItems.count == items.count {
+            for (index, item) in items.enumerate() {
+                let objectToAdd = "\(index + 1). \(quantityOfItems[index]) \(item)"
+                combinedArray.append(objectToAdd)
+            }
+        }
+        return combinedArray
+    }
     
 }
